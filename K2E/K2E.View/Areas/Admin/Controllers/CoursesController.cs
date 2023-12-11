028e3f184b26cc391d57aa1df57ca6eef147f1ba
EduardoVasquez21 <vl0815032020@unab.edu.sv>
2023-12-10 22:36:20 -0600

    InstructoryCourseCRUD

---
+++ K2E/K2E.View/Areas/Admin/Controllers/CoursesController.cs
@@ -0,0 +1,49 @@
+﻿using K2E.DataAcess;
+using K2E.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace K2E.View.Areas.Admin.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Admin/Courses
+        public ActionResult Index()
+        {
+            List<Course> List = CourseDAL.Instance.GetAll();
+
+            return View(List);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Course entity)
+        {
+            CourseDAL.Instance.Create(entity);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Course entity = CourseDAL.Instance.GetById(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Course entity)
+        {
+            CourseDAL.Instance.Update(entity);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ K2E/K2E.View/Areas/Admin/Controllers/InstructoresController.cs
@@ -0,0 +1,50 @@
+﻿using K2E.DataAcess;
+using K2E.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace K2E.View.Areas.Admin.Controllers
+{
+    public class InstructoresController : Controller
+    {
+        // GET: Admin/Instructores
+        public ActionResult Index()
+        {
+            List<Instructor> List = InstructorDAL.Instance.GetAll();
+
+            return View(List);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Instructor entity)
+        {
+            InstructorDAL.Instance.Create(entity);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Instructor entity = InstructorDAL.Instance.GetById(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Instructor entity)
+        {
+            InstructorDAL.Instance.Update(entity);
+            return RedirectToAction("Index");
+        }
+    }
+}
